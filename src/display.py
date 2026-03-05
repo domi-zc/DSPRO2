f47418d3f48c4e5a502689c3911925273b896abc
Noé Felber <noe.felber@gmx.ch>
2026-03-05 20:32:49 +0100

    Add filter for keypoint display

--- src/display.py
+++ src/display.py
@@ -1,13 +1,23 @@
 import cv2
 import mediapipe as mp
+from copy import deepcopy
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
     
-def display_video_with_annotations(frame, key_points):
-    # mp draws directly on the frame, so nothing needs to be returned
+def display_video_with_annotations(frame, key_points, selected_landmarks=range(33)):
     if key_points is not None:
-        mp_drawing.draw_landmarks(frame, key_points, mp_pose.POSE_CONNECTIONS)
+        selected = set(selected_landmarks)
+        kp = deepcopy(key_points)
+        # filter out only wanted landmarks for display
+        for i, landmark in enumerate(kp.landmark):
+            if i not in selected:
+                landmark.visibility = 0
+                landmark.presence = 0
+        connections = [(a, b) for (a, b) in mp_pose.POSE_CONNECTIONS if a in selected and b in selected]
+        
+        # mp draws directly on the frame, so nothing needs to be returned
+        mp_drawing.draw_landmarks(frame, kp, connections)
     
     cv2.imshow("video", frame)
       
--- src/main.py
+++ src/main.py
@@ -11,7 +11,9 @@ def main():
         frame = sf.get_frames()
         result = pe.estimate_pose(frame)
         
-        if display_video_with_annotations(frame, result.pose_landmarks):
+        # Arm keypoints
+        selected_landmarks=range(11,17)
+        if display_video_with_annotations(frame, result.pose_landmarks, selected_landmarks):
             break
         
 if __name__ == "__main__":
